716d59b09ba81584505339d4a8ee3e4ca356de21
Pol MP <pol.moreno.puig@estudiant.upc.edu>
2016-11-19 13:01:29 +0100

    Millorat fumadors. Falta arreglar els casos de cada fumador

--- Examens/Nov2015/fumadors.erl
+++ Examens/Nov2015/fumadors.erl
@@ -1,10 +1,16 @@
 -module(fumadors).
 -compile(export_all).
 
+len([_|T]) -> 1+len(T);
+len([]) -> 0.
+
 procFumador(Tipus) -> receive
-	{Tipus,_}  -> procFumador(Tipus);
+	{Tipus,_} -> procFumador(Tipus);
 	{_,Tipus} -> procFumador(Tipus);
+	{Tipus,Tipus} -> procFumador(Tipus);
+	{A,A} -> io:format("Elements repetits diferents"),procFumador(Tipus);
 	{A,B} -> io:format("Tinc ~p i agafo ~p i ~p~n",[Tipus,A,B]),liar(),fumar(),procFumador(Tipus)
+	
 end.
 
 procProveidor(LlistaPid) -> receive
@@ -15,12 +21,17 @@ end.
 enviaMissatge([Pr],Missatge) -> Pr ! Missatge;
 enviaMissatge([Pr|Ul],Missatge) -> Pr ! Missatge, enviaMissatge(Ul,Missatge).
 
-liar() -> io:format("Liant el cigarret...~n"),timer:sleep(100).
-fumar() -> io:format("Fumant el cigarret...~n"),timer:sleep(200),central!ok.
+liar() -> io:format("Liant el cigarret...~n"),timer:sleep(500).
+fumar() -> io:format("Fumant el cigarret...~n"),timer:sleep(1000),central!ok.
+
+%Genera una tupla amb dos productes (pot sortir repetit)
+generaAleatori(Elements,true) -> {lists:nth(rand:uniform(len(Elements)),Elements), lists:nth(rand:uniform(len(Elements)),Elements)};
+%Genera una tupla amb dos productes (NO pot sortir repetit)
+generaAleatori(Elements,false) -> PrEl=lists:nth(rand:uniform(len(Elements)),Elements),SeEl=lists:nth(rand:uniform(len(Elements)-1),lists:delete(PrEl,Elements)),{PrEl,SeEl}.
+
 
 %Generem dos productes aleatoris
-proveidor(LlistaFumadors) -> Elements=[paper,tabac,llumi], 
-PrEl=lists:nth(rand:uniform(3),Elements),SeEl=lists:nth(rand:uniform(2),lists:delete(PrEl,Elements)),enviaMissatge(LlistaFumadors,{PrEl,SeEl}).
+proveidor(LlistaFumadors) -> Elements=[paper,tabac,llumi], enviaMissatge(LlistaFumadors,generaAleatori(Elements,true)).
 
 creafumador(Tipus) -> spawn(?MODULE, procFumador,[Tipus]).
 
