7098a4f06baec88afadf127125b7cc800cdd0b80
Pol MP <pol.moreno.puig@estudiant.upc.edu>
2016-11-19 12:34:54 +0100

    Acabat fumadors. Opcionalal: afegir protocol que tanqui processos

--- Examens/Nov2015/fumadors.erl
+++ Examens/Nov2015/fumadors.erl
@@ -1,22 +1,27 @@
 -module(fumadors).
--export([start/1]).
+-compile(export_all).
+
+procFumador(Tipus) -> receive
+	{Tipus,_}  -> procFumador(Tipus);
+	{_,Tipus} -> procFumador(Tipus);
+	{A,B} -> io:format("Tinc ~p i agafo ~p i ~p~n",[Tipus,A,B]),liar(),fumar(),procFumador(Tipus)
+end.
+
+procProveidor(LlistaPid) -> receive
+	ok -> proveidor(LlistaPid),procProveidor(LlistaPid)
+end.
 
 % Envia el missatge a tots els PID de la llista
 enviaMissatge([Pr],Missatge) -> Pr ! Missatge;
-enviaMissatge([Pr|Ul],Missatge) -> Pr ! Missatge + enviaMissatge(Ul,Missatge).
+enviaMissatge([Pr|Ul],Missatge) -> Pr ! Missatge, enviaMissatge(Ul,Missatge).
 
-liar() -> timer:sleep()
-
-procFumador(Tipus) -> receive
-  {Tipus,B} -> procFumador(Tipus);
-  {A,Tipus} -> procFumador(Tipus);
-  {A,B} -> 
-end.
+liar() -> io:format("Liant el cigarret...~n"),timer:sleep(100).
+fumar() -> io:format("Fumant el cigarret...~n"),timer:sleep(200),central!ok.
 
 %Generem dos productes aleatoris
-proveidor(LlistaFumadors) -> Elements={paper,tabac,llumi}, 
-PrEl=lists:sth(rand:uniform(3),Elements),SeEl=lists:sth(rand:uniform(2),lists:remove(PrEl,Elements)),enviaMissatge(LlistaFumadors,{PrEl,SeEl}).
+proveidor(LlistaFumadors) -> Elements=[paper,tabac,llumi], 
+PrEl=lists:nth(rand:uniform(3),Elements),SeEl=lists:nth(rand:uniform(2),lists:delete(PrEl,Elements)),enviaMissatge(LlistaFumadors,{PrEl,SeEl}).
 
 creafumador(Tipus) -> spawn(?MODULE, procFumador,[Tipus]).
 
-start() -> proveidor([creafumador(paper),creafumador(tabac),creafumador(llumi)]).
+start() -> LlistaPid=[creafumador(paper),creafumador(tabac),creafumador(llumi)],register(central,spawn(?MODULE, procProveidor,[LlistaPid])),central ! ok. %%Enviem el primer missatge
