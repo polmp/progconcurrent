f9857cd389b411b32aa958e8466c512578533463
Pol <pol.moreno.puig@estudiant.upc.edu>
2016-11-25 19:17:43 +0100

    Gran actualització. Ara el ascensor té memòria i ilumina els botons que anirà amb ordre. Falta arreglar bug si en l'últim pis que anem apretem el botó dos cops o més

--- pr5/ascensor.erl
+++ pr5/ascensor.erl
@@ -1,6 +1,14 @@
 -module(ascensor).
 -import(motor,[start/1]).
--export([start/0,ascensorProc/1,ascensorProc/2,ascensorProc/3,estatReset/1]).
+-export([start/0,ascensorProc/1,ascensorProc/3,estatReset/1]).
+
+removeLast([]) -> [];
+removeLast([_]) -> [] ++ removeLast([]);
+removeLast([A|B]) -> [A] ++ removeLast(B).
+
+checkDifferentInList(A,[A|G]) -> checkDifferentInList(A,G);
+checkDifferentInList(_,List) -> List.
+
 
 run_up() -> motor ! run_up.
 run_down() -> motor ! run_down.
@@ -9,21 +17,46 @@ light_off(P) -> botonera ! {light_off,P}.
 light_on(P) -> botonera ! {light_on,P}.
 display(P) -> botonera ! {display,P}.
 kill(Var) -> Var ! kill.
+killAll() -> stop(),kill(motor),aborted.
 
-ascensorProc(e0,down,Button) -> light_on(Button), run_down(), ascensorProc(e1,Button);
+ascensorProc(e0,down,Button) -> light_on(Button), run_down(), ascensorProc(e1,Button,[]);
 
-ascensorProc(e0,up,Button) -> light_on(Button), run_up(), ascensorProc(e1,Button).
+ascensorProc(e0,up,Button) -> light_on(Button), run_up(), ascensorProc(e1,Button,[]);
 
-ascensorProc(e1,Button) -> receive
-	{sens_pl, Button} -> stop(),display(Button),light_off(Button), ascensorProc(Button);
-	{sens_pl, K} -> display(K), ascensorProc(e1,Button);
-	abort -> stop(),kill(motor),aborted
+ascensorProc(e1,Button,[]) -> receive
+	{sens_pl, Button} -> stop(),io:format("No hi ha pisos pendents, acabem amb ~p~n",[Button]),display(Button),light_off(Button), ascensorProc(Button);
+	{sens_pl, K} -> display(K), ascensorProc(e1,Button,[]);
+	{clicked,N} -> light_on(N),ascensorProc(e1,Button,[N]);
+	abort -> killAll()
 	%A -> io:format("MISSATGE DESCONEGUT: ~p~n",[A]),ascensorProc(e1,Button)
+end;
+
+ascensorProc(e1,Button,List) -> receive
+	{sens_pl,Button} -> stop(),light_off(Button),display(Button),timer:sleep(2000),NextFloor=lists:nth(1,lists:reverse(List)),
+		case NextFloor > Button of
+			true -> io:format("Estic al PIS ~p, Desti seguent: ~p~n",[Button,lists:nth(1,lists:reverse(List))]),
+				run_up(), ascensorProc(e1,NextFloor,removeLast(List));
+			false when NextFloor =:= Button -> ListCheckNext = lists:reverse(checkDifferentInList(Button,lists:reverse(List))), 
+				Next = lists:nth(1,lists:reverse(ListCheckNext)), io:format("Hem apretat el mateix, accio -> passem al pis ~p~n",[Next]), 
+				if Next > NextFloor ->
+					run_up(),ascensorProc(e1,Next,removeLast(ListCheckNext));
+				Next < NextFloor ->
+					run_down(),ascensorProc(e1,Next,removeLast(ListCheckNext))
+				end;
+			false -> io:format("Estic al PIS ~p, Desti seguent: ~p~n",[Button,lists:nth(1,lists:reverse(List))]),
+				run_down(),ascensorProc(e1,NextFloor,removeLast(List))
+			end;
+		
+	{sens_pl, K} -> display(K), ascensorProc(e1,Button,List);
+	{clicked,N} -> light_on(N),ascensorProc(e1,Button,[N|List]);
+	abort -> killAll()
 end.
 
+
+
 ascensorProc(BotoAct) -> receive
-	{clicked,K} when K < BotoAct -> ascensorProc(e0,down,K);
-	{clicked,K} when K > BotoAct -> ascensorProc(e0,up,K);
+	{clicked,K} when K < BotoAct -> io:format("Comencem amb el PIS ~p~n",[K]),ascensorProc(e0,down,K);
+	{clicked,K} when K > BotoAct -> io:format("Comencem amb el PIS ~p~n",[K]),ascensorProc(e0,up,K);
 	{clicked,_} -> ascensorProc(BotoAct);
 	kill -> ok;
 	abort -> stop(),kill(motor),ok
@@ -31,17 +64,20 @@ end.
 
 estatReset(e0) -> receive
 	reset -> io:format("Fent reset...~n"), run_down(), estatReset(e1); %Rebem un reset del sensor
+	abort -> killAll();
 	_ -> estatReset(e0)
 end;
 
 estatReset(e1) -> receive
 	at_bottom -> run_up(), estatReset(e2);
 	reset -> io:format("Fent reset...~n"),estatReset(e0);
+	abort -> killAll();
 	_ -> estatReset(e1)
 end;
 
 estatReset(e2) -> receive
-	{sens_pl,0} -> stop(), ascensorProc(0); 
+	{sens_pl,0} -> stop(), ascensorProc(0);
+	abort -> killAll(); 
 	_ -> estatReset(e2)
 end.
 
