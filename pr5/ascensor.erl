a461e8b8cd951bd7a03c91ff1f90bd3d5f5aa0cb
Pol <pol.moreno.puig@estudiant.upc.edu>
2016-11-25 11:03:53 +0100

    Afegit encapsulament de missatges. Arreglat text en mòdul motor

--- pr5/ascensor.erl
+++ pr5/ascensor.erl
@@ -2,22 +2,30 @@
 -import(motor,[start/1]).
 -export([start/0,ascensorProc/1]).
 
+run_up() -> motor ! run_up.
+run_down() -> motor ! run_down.
+stop() -> motor ! stop.
+light_off(P) -> botonera ! {light_off,P}.
+light_on(P) -> botonera ! {light_on,P}.
+display(P) -> botonera ! {display,P}.
+kill(Var) -> Var ! kill.
+
 pushedButton(Button) ->
 	receive
-		at_bottom -> motor ! run_up, pushedButton(0);
-		{sens_pl,P} when Button =:= P -> motor ! stop, botonera ! {light_off,P}, botonera ! {display,P}, ascensorProc(P);
-		{sens_pl,P} -> botonera ! {display,P}, pushedButton(Button);
-		abort -> motor ! stop, motor ! kill, ascensor ! kill %Rebem abort de la botonera quan estem fent. Motor ja fa kill de sensor
+		at_bottom -> run_up(), pushedButton(0);
+		{sens_pl,P} when Button =:= P -> stop(), light_off(P), display(P), ascensorProc(P);
+		{sens_pl,P} -> display(P), pushedButton(Button);
+		abort -> stop(), kill(motor), kill(ascensor) %Rebem abort de la botonera quan estem fent. Motor ja fa kill de sensor
 	end.
 
 ascensorProc(BotoAct) -> receive
-	{clicked,K} when K < BotoAct -> botonera ! {light_on,K}, motor ! run_down, pushedButton(K);
-	{clicked,K} when K > BotoAct -> botonera ! {light_on,K}, motor ! run_up, pushedButton(K);
+	{clicked,K} when K < BotoAct -> light_on(K), run_down(), pushedButton(K);
+	{clicked,K} when K > BotoAct -> light_on(K), run_up(), pushedButton(K);
 	{clicked,_} -> ascensorProc(BotoAct);
-	at_bottom -> motor ! run_up;
-	reset -> io:format("Fent reset...~n"), motor ! run_down, pushedButton(288);
+	at_bottom -> run_up();
+	reset -> io:format("Fent reset...~n"), run_down(), pushedButton(288);
 	kill -> ok;
-	abort -> motor ! stop, motor ! kill,ok
+	abort -> stop(),kill(motor),ok
 end.
 
 start() -> register(ascensor,spawn(?MODULE, ascensorProc, [1])),register(botonera,bcab:new(4,ascensor)),register(sensor,spawn(sensor, sensorProc,[])),register(motor,spawn(motor, startMotor, [5.5])),sensor ! ready.
--- pr5/sensor.erl
+++ pr5/sensor.erl
@@ -3,6 +3,11 @@
 -import(motor,[calculaAltura/2]).
 -include("motor.hrl").
 
+at_top() -> ascensor ! at_top.
+at_bottom() -> ascensor ! at_bottom.
+reset() -> ascensor ! reset.
+sens_pl(Val) -> ascensor ! {sens_pl,Val}.
+
 %Donada una altura, calcula el sensor que s'ha d'activar. Només envia el missatge 1 cop.
 calculaSensor(_,[]) -> undef;
 calculaSensor(Altura,[{Num, Alt}|Llista]) -> 
@@ -14,12 +19,13 @@ calculaSensor(Altura,[{Num, Alt}|Llista]) ->
 
 sensorProc() -> receive
 	kill -> ok;
-	at_top -> ascensor ! at_top, sensorProc(); %al ascensor
-	at_bottom -> ascensor ! at_bottom, sensorProc();
-	ready -> ascensor ! reset, sensorProc(); %Quan rebem un ready, acabem d'inicialitzar el motor, enviem un reset al ascensor
+	stop -> sensorProc();
+	at_top -> at_top(), sensorProc(); %al ascensor
+	at_bottom -> at_bottom(), sensorProc();
+	ready -> reset(), sensorProc(); %Quan rebem un ready, acabem d'inicialitzar el motor, enviem un reset al ascensor
 	{at,P} -> case calculaSensor(P,[{0,0.5},{1,5.5},{2,8.5},{3,12.5}]) of
 		undef -> sensorProc();
-		{Val,_} -> io:format("EnvioSensorPis -> ~p~n",[Val]),ascensor ! {sens_pl,Val},sensorProc()  %Enviem pid al ascensor 
+		{Val,_} -> io:format("EnvioSensorPis -> ~p~n",[Val]),sens_pl(Val),sensorProc()  %Enviem pid al ascensor 
 	end 
 end.
 
