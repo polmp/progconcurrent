70cc8584a928666ae977bebef7d465d09767a61b
Pol <pol.moreno.puig@estudiant.upc.edu>
2016-11-27 13:17:14 +0100

    Modificat ascensor per interactuar amb pisos. Falta arreglar errors

--- pr5/ascensor.erl
+++ pr5/ascensor.erl
@@ -1,6 +1,7 @@
 -module(ascensor).
 -import(motor,[start/1]).
--export([start/0,ascensorProc/1,ascensorProc/3,estatReset/1,checkDifferentInList/2]).
+-import(bppool,[display/2,set_light/3,kill/0,envia_a_tots_excepte/3]).
+-export([start/0,ascensorProc/1,abort/0,ascensorProc/3,estatReset/1,checkDifferentInList/2,pushed/1]).
 
 removeLast([]) -> [];
 removeLast([_]) -> [] ++ removeLast([]);
@@ -17,15 +18,17 @@ light_off(P) -> botonera ! {light_off,P}.
 light_on(P) -> botonera ! {light_on,P}.
 display(P) -> botonera ! {display,P}.
 kill(Var) -> Var ! kill.
-killAll() -> stop(),kill(motor),aborted.
+killAll() -> stop(),kill(motor),kill(),aborted.
+abort() -> ascensor!abort.
+pushed(Pis) -> ascensor ! {clicked,Pis}. 
 
 ascensorProc(e0,down,Button) -> light_on(Button), run_down(), ascensorProc(e1,Button,[]);
 
 ascensorProc(e0,up,Button) -> light_on(Button), run_up(), ascensorProc(e1,Button,[]);
 
 ascensorProc(e1,Button,[]) -> receive
-	{sens_pl, Button} -> stop(),io:format("No hi ha pisos pendents, acabem amb ~p~n",[Button]),display(Button),light_off(Button), ascensorProc(Button);
-	{sens_pl, K} -> display(K), ascensorProc(e1,Button,[]);
+	{sens_pl, Button} -> stop(),io:format("No hi ha pisos pendents, acabem amb ~p~n",[Button]),display(Button),light_off(Button), set_light(Button,all,off),display(Button,"HERE"),envia_a_tots_excepte(display,Button,Button),ascensorProc(Button);
+	{sens_pl, K} -> display(K), display(Button,K),ascensorProc(e1,Button,[]);
 	{clicked,N} -> light_on(N),ascensorProc(e1,Button,[N]);
 	abort -> killAll()
 	%A -> io:format("MISSATGE DESCONEGUT: ~p~n",[A]),ascensorProc(e1,Button)
@@ -50,17 +53,17 @@ ascensorProc(e1,Button,List) -> receive
 				run_down(),ascensorProc(e1,NextFloor,removeLast(List))
 			end;
 		
-	{sens_pl, K} -> display(K), ascensorProc(e1,Button,List);
+	{sens_pl, K} -> display(K), display(Button,K),ascensorProc(e1,Button,List);
 	{clicked,N} -> light_on(N),ascensorProc(e1,Button,[N|List]);
 	abort -> killAll()
 end.
 
 ascensorProc(BotoAct) -> receive
-	{clicked,K} when K < BotoAct -> io:format("Comencem amb el PIS ~p~n",[K]),ascensorProc(e0,down,K);
-	{clicked,K} when K > BotoAct -> io:format("Comencem amb el PIS ~p~n",[K]),ascensorProc(e0,up,K);
+	{clicked,K} when K < BotoAct -> io:format("Comencem amb el PIS ~p~n",[K]),set_light(K,all,on),envia_a_tots_excepte(display,"BUSY",K),ascensorProc(e0,down,K);
+	{clicked,K} when K > BotoAct -> io:format("Comencem amb el PIS ~p~n",[K]),set_light(K,all,on),envia_a_tots_excepte(display,"BUSY",K),ascensorProc(e0,up,K);
 	{clicked,_} -> ascensorProc(BotoAct);
 	kill -> ok;
-	abort -> stop(),kill(motor),ok
+	abort -> killAll()
 end.
 
 estatReset(e0) -> receive
@@ -83,7 +86,7 @@ estatReset(e2) -> receive
 end.
 
 
-start() -> register(ascensor,spawn(?MODULE, estatReset, [e0])),register(botonera,bcab:new(4,ascensor)),register(sensor,spawn(sensor, sensorProc,[])),register(motor,spawn(motor, startMotor, [5.5])),sensor ! ready,initial_reset. 
+start() -> register(ascensor,spawn(?MODULE, estatReset, [e0])),register(botonera,bcab:new(4,ascensor)),register(sensor,spawn(sensor, sensorProc,[])),register(motor,spawn(motor, startMotor, [5.5])),sensor ! ready,initial_reset, wxenv:start(), bppool:start(3). 
 
 
 
