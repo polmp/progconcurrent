2076f05230978ec5c810d134e656507ee7eeccab
Pol <pol.moreno.puig@estudiant.upc.edu>
2016-11-28 08:31:11 +0100

    Arreglat tatancament de processos

--- pr5/ascensor.erl
+++ pr5/ascensor.erl
@@ -65,25 +65,25 @@ ascensorProc(BotoAct) -> receive
 	{clicked,K} when K > BotoAct -> io:format("Comencem amb el PIS ~p~n",[K]),set_light(K,all,on),envia_a_tots_excepte(display,"BUSY",K),ascensorProc(e0,up,K);
 	{clicked,_} -> ascensorProc(BotoAct);
 	kill -> ok;
-	abort -> killAll(),bbpool:kill()
+	abort -> killAll(),bppool:kill(),wxenv!kill
 end.
 
 estatReset(e0) -> receive
 	reset -> io:format("Fent reset...~n"), run_down(), estatReset(e1); %Rebem un reset del sensor
-	abort -> killAll(),bbpool:kill();
+	abort -> killAll(),bppool:kill(),wxenv!kill;
 	_ -> estatReset(e0)
 end;
 
 estatReset(e1) -> receive
 	at_bottom -> run_up(), estatReset(e2);
 	reset -> io:format("Fent reset...~n"),estatReset(e0);
-	abort -> killAll(),bbpool:kill();
+	abort -> killAll(),bppool:kill(),wxenv!kill;
 	_ -> estatReset(e1)
 end;
 
 estatReset(e2) -> receive
 	{sens_pl,0} -> stop(), ascensorProc(0);
-	abort -> killAll(); 
+	abort -> killAll(), bppool:kill(),wxenv!kill; 
 	_ -> estatReset(e2)
 end.
 
